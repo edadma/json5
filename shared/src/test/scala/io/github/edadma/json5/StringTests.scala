0476359106d2b2deb8ecd025409088571bb82a2b
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-10 11:10:00 -0400

    unit tests

---
+++ shared/src/test/scala/io/github/edadma/json5/StringTests.scala
@@ -0,0 +1,10 @@
+package io.github.edadma.json5
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class StringTests extends AnyFreeSpec with Matchers:
+
+  "number" in {
+    parseFromString(" 123 ") shouldBe NumberValue("123")
+  }
