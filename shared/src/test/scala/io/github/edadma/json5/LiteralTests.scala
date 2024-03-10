a4f0df2bb141503f2e6e19be0f65241fbd7bb687
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-10 11:08:45 -0400

    unit tests

--- shared/src/test/scala/io/github/edadma/json5/LiteralTests.scala
+++ shared/src/test/scala/io/github/edadma/json5/LiteralTests.scala
@@ -3,11 +3,7 @@ package io.github.edadma.json5
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class Tests extends AnyFreeSpec with Matchers:
-
-  "number" in {
-    parseFromString(" 123 ") shouldBe NumberValue("123")
-  }
+class LiteralTests extends AnyFreeSpec with Matchers:
 
   "null" in {
     parseFromString(" null ") shouldBe NullValue
