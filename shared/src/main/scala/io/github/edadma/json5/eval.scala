52a80a7f1d45e536e5b8dd1a55b13b52171284e6
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-10 20:20:02 -0400

    eval

--- shared/src/main/scala/io/github/edadma/json5/eval.scala
+++ shared/src/main/scala/io/github/edadma/json5/eval.scala
@@ -1,6 +1,16 @@
 package io.github.edadma.json5
 
+import scala.collection.immutable.ListMap
+
 def eval(v: Value): Any =
   v match
-    case NumberValue(n)      => n.toDouble
-    case HexadecimalValue(h) => Integer.parseInt(h, 16)
+    case NumberValue(n)        => n.toDouble
+    case HexadecimalValue(h)   => Integer.parseInt(h, 16)
+    case BooleanValue(b)       => b
+    case StringValue(s)        => s
+    case ArrayValue(a)         => a map eval
+    case ObjectValue(m)        => (m.view mapValues eval) to ListMap
+    case NullValue             => null
+    case InfinityValue         => Double.PositiveInfinity
+    case NegativeInfinityValue => Double.NegativeInfinity
+    case NaNValue              => Double.NaN
--- shared/src/test/scala/io/github/edadma/json5/Main.scala
+++ shared/src/test/scala/io/github/edadma/json5/Main.scala
@@ -4,7 +4,19 @@ import pprint.pprintln
 
 @main def run(): Unit =
   val v = parseFromString(
-    raw""" 0x123 """,
+    raw"""
+         |{
+         |  // comments
+         |  unquoted: 'and you can quote me on that',
+         |  singleQuotes: 'I can use "double quotes" here',
+         |  lineBreaks: "Look, Mom! \
+         |No \\n's!",
+         |  hexadecimal: 0xdecaf,
+         |  leadingDecimalPoint: .8675309, andTrailing: 8675309.,
+         |  positiveSign: +1,
+         |  trailingComma: 'in objects', andIn: ['arrays',],
+         |  "backwardsCompatible": "with JSON",
+         |}""".stripMargin,
   )
 
   pprintln(v)
